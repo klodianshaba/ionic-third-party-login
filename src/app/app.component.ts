d2dc17e699bb439ff65250606b756d77b1c918d4
klodian <dev.klodian@gmail.com>
2021-03-18 22:05:52 +0100

    sign in refresh token and log out from third party google

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,4 @@
 import { Component } from '@angular/core';
-// import { GooglePlus } from '@ionic-native/google-plus/ngx';
-import {environment} from '../environments/environment';
-// const googleWebClientId = environment.googleWebClientId;
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
@@ -9,11 +6,4 @@ import {environment} from '../environments/environment';
 })
 export class AppComponent {
   constructor() {}
-
-  // onGoogleLogin(): void {
-  //   this.googlePlus.login({webClientId: googleWebClientId} // optional clientId of your Web application from Credentials settings of your project - On Android, this MUST be included to get an idToken. On iOS, it is not required.
-  //   ).then(res => {
-  //     console.log(res);
-  //   }).catch(err => console.error(err));
-  // }
 }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
-
+import '@codetrix-studio/capacitor-google-auth';
+import { Plugins } from '@capacitor/core';
+const  { GoogleAuth } = Plugins;
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
+  public user = null;
   constructor() {}
+
+  async googleSignup() {
+
+    const googleUser = await GoogleAuth.signIn() as any;
+    console.log('my user: ', googleUser);
+    this.user = googleUser;
+  }
+
+  async signOut() {
+    await GoogleAuth.signOut();
+    this.user = null;
+  }
+
+  async refreshToken() {
+    const response = await GoogleAuth.refresh();
+    console.log( "refresh: ", response);
+  }
+
 
 }
