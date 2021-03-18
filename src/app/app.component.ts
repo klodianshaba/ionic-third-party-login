61228ea8e90c3c81ccd9cd173b033b8ceec2ef51
klodian <dev.klodian@gmail.com>
2021-03-18 12:30:55 +0100

    added ios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,19 @@
 import { Component } from '@angular/core';
-import { GooglePlus } from '@ionic-native/google-plus/ngx';
+// import { GooglePlus } from '@ionic-native/google-plus/ngx';
 import {environment} from '../environments/environment';
-const googleWebClientId = environment.googleWebClientId;
+// const googleWebClientId = environment.googleWebClientId;
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor(private googlePlus: GooglePlus) {}
+  constructor() {}
 
-  onGoogleLogin(): void {
-    this.googlePlus.login({webClientId: googleWebClientId} // optional clientId of your Web application from Credentials settings of your project - On Android, this MUST be included to get an idToken. On iOS, it is not required.
-    ).then(res => {
-      console.log(res);
-    }).catch(err => console.error(err));
-  }
+  // onGoogleLogin(): void {
+  //   this.googlePlus.login({webClientId: googleWebClientId} // optional clientId of your Web application from Credentials settings of your project - On Android, this MUST be included to get an idToken. On iOS, it is not required.
+  //   ).then(res => {
+  //     console.log(res);
+  //   }).catch(err => console.error(err));
+  // }
 }
